2a0162661d509e6da87a0e553682de27e7e3e78f
Iago Carvalho <iagocarvalhoweb@icloud.com>
2019-04-03 15:39:01 -0300

    configurando a tela de navegação

--- src/pages/browse/index.js
+++ src/pages/browse/index.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import { Container, List, Playlist, Title } from './styled';
 
-const Browser=() => <h1>Hello World</h1>;
+const Browser=() => (
+  <Container>
+    <Title>Navegar</Title>
+
+    <List>
+      <Playlist href="">
+        <img src="https://static3.tcdn.com.br/img/img_prod/224611/capital_inicial_via_brasil_27_04_18_americana_sp_16479_1_20180321180432.jpg" alt="Playlist" />
+        <strong>Melhores do Rock</strong>
+        <p>Relaxe enquanto você programa ouvindo apenas as melhores do rock mundial!</p>
+      </Playlist>
+    </List>
+  </Container>
+)
 
 export default Browser;
