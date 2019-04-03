a29b7b1e0d77ac62af13fef188e701a27a0e6bdb
Iago Carvalho <iagocarvalhoweb@icloud.com>
2019-04-03 17:03:22 -0300

    config reactotron

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
 import Player from './components/Player';
 import Sidebar from './components/Sidebar';
+import './config/reactotron';
 import Routes from './routes';
 import { Container, Content, Wrapper } from './styles/component';
 import { GlobalStyle } from './styles/global';
