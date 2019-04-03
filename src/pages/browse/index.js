e6e55cb77c127ddeb86c1cff7e58e880baa54a4b
Iago Carvalho <iagocarvalhoweb@icloud.com>
2019-04-03 15:40:24 -0300

    replicando a playlist

--- src/pages/browse/index.js
+++ src/pages/browse/index.js
@@ -11,6 +11,24 @@ const Browser=() => (
         <strong>Melhores do Rock</strong>
         <p>Relaxe enquanto você programa ouvindo apenas as melhores do rock mundial!</p>
       </Playlist>
+
+      <Playlist href="">
+        <img src="https://static3.tcdn.com.br/img/img_prod/224611/capital_inicial_via_brasil_27_04_18_americana_sp_16479_1_20180321180432.jpg" alt="Playlist" />
+        <strong>Melhores do Rock</strong>
+        <p>Relaxe enquanto você programa ouvindo apenas as melhores do rock mundial!</p>
+      </Playlist>
+
+      <Playlist href="">
+        <img src="https://static3.tcdn.com.br/img/img_prod/224611/capital_inicial_via_brasil_27_04_18_americana_sp_16479_1_20180321180432.jpg" alt="Playlist" />
+        <strong>Melhores do Rock</strong>
+        <p>Relaxe enquanto você programa ouvindo apenas as melhores do rock mundial!</p>
+      </Playlist>
+
+      <Playlist href="">
+        <img src="https://static3.tcdn.com.br/img/img_prod/224611/capital_inicial_via_brasil_27_04_18_americana_sp_16479_1_20180321180432.jpg" alt="Playlist" />
+        <strong>Melhores do Rock</strong>
+        <p>Relaxe enquanto você programa ouvindo apenas as melhores do rock mundial!</p>
+      </Playlist>
     </List>
   </Container>
 )
